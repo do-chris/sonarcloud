9d8176f8fc8b7716663794f89e4af262cb82f667
Christopher Do <christopherdo@AU-MAC-FVFDF0TMP3YV.home.local>
2021-07-29 19:51:14 +1000

    commit

--- sonar.sh
+++ sonar.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+pylint -ry *.py > pylint-report.txt
+coverage run -m pytest *.py
+coverage xml *.py
+
 sonar-scanner \
   -Dsonar.projectKey=localtest \
   -Dsonar.sources=. \
@@ -5,3 +11,7 @@ sonar-scanner \
   -Dsonar.python.coverage.reportPaths=coverage.xml \
   -Dsonar.python.pylint.reportPaths=pylint-report.txt \
   -Dsonar.login=5f3deea7af9e3fa788d8f423129aa60e433b81cf
+
+rm -rf pylint-report.txt
+rm -rf .coverage coverage.xml
+rm -rf __py*
